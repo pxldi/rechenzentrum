3e37619c6bb9c9f40374edca34a2b3e5a5866d58
Leopold Sappler <89982382+pxldi@users.noreply.github.com>
2026-02-12 21:11:26 +0100

    Feature/nixos config (#5)
    * feat(nix): add hardware configuration
    
    * feat(nix): include uuids for storage and ssh key

--- nix/modules/storage.nix
+++ nix/modules/storage.nix
@@ -2,13 +2,13 @@
 
 {
   # fileSystems."/mnt/hdd1" = {
-  #   device = "/dev/disk/by-uuid/YOUR-HDD1-UUID";
+  #   device = "/dev/disk/by-uuid/a00c073a-8d62-4da2-b5d1-b67b3f485aff";
   #   fsType = "ext4";
   #   options = [ "nofail" ];
   # };
   # 
   # fileSystems."/mnt/hdd2" = {
-  #   device = "/dev/disk/by-uuid/YOUR-HDD2-UUID";
+  #   device = "/dev/disk/by-uuid/13864c23-ea60-441c-a9be-358f3a1042b4";
   #   fsType = "ext4";
   #   options = [ "nofail" ];
   # };
